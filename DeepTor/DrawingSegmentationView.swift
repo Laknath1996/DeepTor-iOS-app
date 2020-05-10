a1627b7533bddd09e5efbeaad5b4822280738d71
Laknath1996 <ashwindesilva@yahoo.com>
2020-05-10 16:11:20 +0530

    deleting some files

--- DeepTor/DrawingSegmentationView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DrawingSegmentationView.swift
-//  DeepTor
-//
-//  Created by Ashwin de Silva on 5/10/20.
-//  Copyright © 2020 Ashwin de Silva. All rights reserved.
-//
-
-import Foundation
